45a9aa133431bb22a04039d02ea9740ee2309388
Nitsarut Beadnok (Game) <nitsarut@tectony.co.th>
2021-07-16 13:11:40 +0700

    close tag

--- app/components/product/EditerView.js
+++ app/components/product/EditerView.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+/* import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import dynamic from "next/dynamic";
 
 const Editor = dynamic(
   () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
   { ssr: false }
-);
+); */
 
 const EditerView = (props) => {
  /*  const [contentstate, setContentstate] = useState({});
