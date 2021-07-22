4c51a427576a814bf0e79c0377f77d8f3c6c848e
Nitsarut Beadnok (Game) <nitsarut@tectony.co.th>
2021-07-22 14:53:45 +0700

    update redux

--- app/context/store.js
+++ app/context/store.js
@@ -1,12 +1,19 @@
-import { createStore, applyMiddleware, Store, AnyAction } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 import { createWrapper, Context, HYDRATE } from "next-redux-wrapper";
 import thunk from "redux-thunk";
 import Reducers from "./reducers";
 
+let middleware = [];
+if (process.env.NODE_ENV === "development") {
+  middleware = [...middleware, thunk, logger];
+} else {
+  middleware = [...middleware, thunk];
+}
+
 /* export interface State {
     tick: string;
 }
  */
-const makeStore = () => createStore(Reducers, applyMiddleware(logger, thunk));
+const makeStore = () => createStore(Reducers, applyMiddleware(...middleware));
 export const wrapper = createWrapper(makeStore, { debug: true });
